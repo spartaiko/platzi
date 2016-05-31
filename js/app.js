613767034e844cad0d08adb3186f9a44e11cb788
spartaiko <juanchy@gmail.com>
2016-05-31 11:50:07 -0300

    act. FINAL

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
-var Usuario = React.createClass ({
-    propTypes: {
-        nombres : React.PropTypes.string.isRequired,
-        apellido : React.PropTypes.string.isRequired
-    },
-    render: function() {
-        return <span>Hola {this.props.nombre} {this.props.apellido}</span>
-    }    
-});
+'use strict';
 
 var app = angular.module('app', ['react']);
 
-var User_ctrl = function(){
-    this.usuario = {
-        nombre : 'Chuck',
-        apellido : 'Norris'
-    }
-}
-app.controller( 'user_ctrl', User_ctrl);
+var Usuario = React.createClass({
+    displayName: 'Usuario',
 
-app.directive( 'usuario', function(reactDirective){
+    propTypes: {
+        nombre: React.PropTypes.string.isRequired,
+        apellido: React.PropTypes.string.isRequired
+    },
+    render: function render() {
+        return React.createElement(
+            'span',
+            null,
+            'Hola ',
+            this.props.nombre,
+            ' ',
+            this.props.apellido
+        );
+    }
+});
+
+var User_ctrl = function User_ctrl() {
+    this.usuario = {
+        nombre: 'Concha',
+        apellido: 'Tu Madre'
+    };
+};
+app.controller('user_ctrl', User_ctrl);
+
+app.directive('usuario', function (reactDirective) {
     return reactDirective(Usuario);
-})
+});
--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,23 +1,23 @@
+var app = angular.module('app', ['react']);
+
 var Usuario = React.createClass ({
     propTypes: {
-        nombres : React.PropTypes.string.isRequired,
+        nombre : React.PropTypes.string.isRequired,
         apellido : React.PropTypes.string.isRequired
     },
     render: function() {
-        return <span>Hola {this.props.nombre} {this.props.apellido}</span>
+        return <span>Hola {this.props.nombre} {this.props.apellido}</span>;
     }    
 });
 
-var app = angular.module('app', ['react']);
-
 var User_ctrl = function(){
     this.usuario = {
-        nombre : 'Chuck',
-        apellido : 'Norris'
+        nombre : 'Concha',
+        apellido : 'Tu Madre'
     }
 }
 app.controller( 'user_ctrl', User_ctrl);
 
 app.directive( 'usuario', function(reactDirective){
     return reactDirective(Usuario);
-})
+});
